9ab05716252d6b43727d0ffd84c434a8589ef974
Lev Gridnev <cppovr@gmail.com>
2017-11-05 22:10:41 +0300

    parser v0.2

--- modules/parser.js
+++ modules/parser.js
@@ -1,53 +1,54 @@
-const cheerio = require('cheerio');
-const pdfText = require('pdf-text');
-const WordExtractor = require("word-extractor");
-const mammoth = require("mammoth");
-const fs = require("fs");
-const tempfile = require('tempfile');
+(function() {
+	const cheerio = require('cheerio');
+	const pdfText = require('pdf-text');
+	const WordExtractor = require("word-extractor");
+	const mammoth = require("mammoth");
+	const fs = require("fs");
+	const tempfile = require('tempfile');
 
-module.exports.extractText   = extractText   = function(str, mime) {
-	if (mime == "text/html" || mime == "html") {
-		return new Promise(function(resolve, reject) {
-			resolve(textFromHTML(str));
+	var norm = str => str.replace(/\s+/g,' ').trim();
+	var m2e = mime => {
+		if (/^text\/html/i.test(mime)) return "html";
+		if (mime == "application/pdf") return "pdf";
+		if (mime == "application/msword") return "doc";
+		if (mime == "application/vnd.openxmlformats-officedocument.wordprocessingml.document") return "docx";
+		return ""
+	}
+
+	module.exports.getLinks = function(buffer) {
+		var $ = cheerio.load(buffer);
+		return $("a[href]").map((_, el) => $(el).attr("href")).get();
+	};
+	module.exports.extractText = function(buffer, mime) {
+		return new Promise((resolve, reject) => {
+			switch (m2e(mime)) {
+				case "html":
+					var $ = cheerio.load(buffer);
+					resolve(norm($('html').text()));
+					break;
+				case "pdf":
+					pdfText(buffer, function(err, chunks) {
+						if (err) reject(err);
+						else resolve(norm(chunks.join(" ")));
+					});
+					break;
+				case "doc":
+					var file = tempfile(".doc");
+					fs.writeFileSync(file, buffer);
+					var extractor = new WordExtractor();
+					resolve(extractor.extract(file).then(doc => {
+						fs.unlinkSync(file);
+						return norm(doc.getBody());
+					}));
+					break;
+				case "docx":
+					var doc = {buffer: buffer};
+					resolve(mammoth.extractRawText(doc)
+						.then(res => norm(res.value)));
+					break;
+				default:
+					resolve("");
+			}
 		});
-	}
-	if (mime == "application/pdf" || mime == "pdf") {
-		return textFromPDF(str);
-	}
-	if (mime == "application/msword" || mime == "doc") {
-		return textFromDOC(str);
-	}
-	if (mime == "application/vnd.openxmlformats-officedocument.wordprocessingml.document" || mime == "docx") {
-		return textFromDOCX(str);
-	}
-};
-module.exports.textFromHTML  = textFromHTML  = function(str) {
-	var $ = cheerio.load(str);
-	return $('html').text().replace(/\s+/g,' ').trim();
-};
-module.exports.linksFromHTML = linksFromHTML = function(str) {
-	var $ = cheerio.load(str);
-	return $("a[href]").map((_, el) => $(el).attr("href")).get();
-};
-module.exports.textFromPDF   = textFromPDF   = function(str) {
-	return new Promise(function (resolve, reject) {
-		pdfText(str, function(err, chunks) {
-			if (err) reject(err);
-			else resolve(chunks.join(" ").replace(/\s+/g,' ').trim());
-		})
-	});
-};
-module.exports.textFromDOC   = textFromDOC   = function(str) {
-	var file = tempfile(".doc");
-	fs.writeFileSync(file, str);
-	var extractor = new WordExtractor();
-	return extractor.extract(file).then(doc => {
-		fs.unlinkSync(file);
-		return doc.getBody().replace(/\s+/g,' ').trim();
-	});
-};
-module.exports.textFromDOCX  = textFromDOCX  = function(str) {
-	return mammoth.extractRawText({buffer: str}).then(function(result){
-		return result.value.replace(/\s+/g,' ').trim();
-	});
-};
+	};
+})();
